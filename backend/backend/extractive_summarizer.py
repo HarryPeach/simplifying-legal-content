5c2b0f2fba05ecb27ba7aff59f573cad766b837f
Harry Peach <4750998+HarryPeach@users.noreply.github.com>
2022-04-26 13:46:52 +0100

    Add logic to extractive summarizer

--- backend/backend/extractive_summarizer.py
+++ backend/backend/extractive_summarizer.py
@@ -2,12 +2,14 @@ import scipy
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from nltk.tokenize import sent_tokenize
+from torch import threshold
 
 
 class ExtractiveSummarizer():
-    def __init__(self):
-        self.sentence_model = SentenceTransformer("all-mpnet-base-v2")
-        self.embeddings = np.load("models/extractives/embeddings.npz")["arr_0"]
+    def __init__(self, threshold, embeddings_path="models/extractive/embeddings.npz", model="all-mpnet-base-v2"):
+        self.sentence_model = SentenceTransformer(model)
+        self.embeddings = np.load(embeddings_path)["arr_0"]
+        self.threshold = threshold
 
     def _get_embeddings(self, sentences: list[str]) -> list[list[int]]:
         """Returns the embeddings for a given list of sentences
@@ -20,16 +22,23 @@ class ExtractiveSummarizer():
         """
         return self.sentence_model.encode(sentences)
 
-    def _classify(self, sent: str) -> bool:
+    def _classify(self, embedding: list[int]) -> bool:
         """Takes a sentence as returns whether or not it should be kept
 
         Args:
             sent (str): The sentence to classify
+            embedding (list[int]): The embedding for the sentence
 
         Returns:
             bool: Whether the sentence should be kept in the summary
         """
-        ...
+        distances = scipy.spatial.distance.cdist(
+            [embedding], self.embeddings, "cosine")[0]
+
+        results = zip(range(len(distances)), distances)
+        results = sorted(results, key=lambda x: x[1])
+
+        return True if results[0][1] > threshold else False
 
     def summarise(self, doc: str) -> list[str]:
         """Summarises a document using the extractive summariser
@@ -41,3 +50,10 @@ class ExtractiveSummarizer():
             list[str]: The resulting sentences, in a list
         """
         sent_tokens = sent_tokenize(doc)
+
+        classified: list[str] = []
+        for text, embdg in zip(sent_tokens, self._get_embeddings(sent_tokens)):
+            if self._classify(text, embdg):
+                classified.append(text)
+
+        return classified
