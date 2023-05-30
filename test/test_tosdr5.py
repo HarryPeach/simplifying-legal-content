6355ac7e71c34acad98055da05a171e7edd3ed94
Nicolay Rusnachenko <kolyarus@yandex.ru>
2023-05-30 17:23:58 +0100

    Example of extractive evaluation

--- test/test_tosdr5.py
+++ test/test_tosdr5.py
@@ -1,6 +1,7 @@
 import unittest
-
 from backend.extractive_summarizer import ExtractiveSummariser
+from rouge_score import rouge_scorer
+from tqdm import tqdm
 
 from src.tosdr5 import iter_tosdr5_texts
 
@@ -15,7 +16,29 @@ class TestTOSDR5(unittest.TestCase):
             embeddings_path="../backend/backend/models/extractive/embeddings.npz",
             cache_folder=TestTOSDR5.cache_dir)
 
-        for text in iter_tosdr5_texts():
-            print(text)
-            sentences = summarizer.summarise(text, threshold=0.725)
-            print(sentences)
+        raw_texts = iter_tosdr5_texts(type="raw")
+        summ_texts = iter_tosdr5_texts(type="summ")
+
+        pred_texts = [" ".join(summarizer.summarise(raw_text, threshold=0.725))
+                      for raw_text in tqdm(raw_texts, desc="Extractive model")]
+
+        # Score.
+        scores = []
+        scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
+        for t_pred, t_sum in zip(pred_texts, summ_texts):
+            scores.append(scorer.score(target=t_sum, prediction=t_pred))
+
+        r = {}
+
+        for s in scores:
+            for v in s:
+                if v not in r:
+                    r[v] = {"p": [], "r": [], "f": []}
+                r[v]["p"].append(s[v][0])
+                r[v]["r"].append(s[v][1])
+                r[v]["f"].append(s[v][2])
+
+        for m in ["rouge1", "rouge2", "rougeL"]:
+            print(sum(r[m]["p"])/len(r[m]["p"]),
+                  sum(r[m]["r"])/len(r[m]["r"]),
+                  sum(r[m]["f"])/len(r[m]["f"]))
